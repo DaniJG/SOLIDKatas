58ebb5478387d8b3e7fbbd06d174cc2a4492c0da
Konstantin Ilin <konstantin.ilin@ericsson.com>
2016-01-27 14:14:36 +0000

    Adding Liskov Substitution Principle Kata

---
+++ SOLID.Katas/LSP/Customer.cs
@@ -0,0 +1,30 @@
+﻿
+namespace SOLID.Katas.LSP
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Customer
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public List<Guid> OrdersIds { get; set; }
+
+        public Customer(Guid id, string name, string email, List<Guid> orders)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Email = email;
+            this.OrdersIds = orders;
+        }
+
+        public virtual int CalculateDiscount()
+        {
+            return OrdersIds.Count > 1 ? 25 : 15;
+        }
+    }
+}
---
+++ SOLID.Katas/LSP/EmailNotifier.cs
@@ -0,0 +1,18 @@
+﻿
+namespace SOLID.Katas.LSP
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class EmailNotifier
+    {
+        public void SendDiscountNotification(IList<Customer> customers)
+        {
+            foreach (var customer in customers)
+            {
+                var message = string.Format("Sending notification to {0} with {1}% discount.", customer.Email, customer.CalculateDiscount());
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
